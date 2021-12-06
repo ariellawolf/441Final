d7967c96ced5d4531ef1256efdb664419a02acf5
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-12-06 19:36:20 +0000

    Initial commit

---
+++ dispense.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import time
+import threading
+import smbus
+from classes import ADC
+
+GPIO.setmode(GPIO.BCM)
+
+photoResistor= 6 #change as needed
+GPIO.setup(photoResistor, GPIO.IN)
+stepperPins = [18,21,22,23] # controller inputs: in1, in2, in3, in4
+sequence = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
+        [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+for pin in stepperPins:
+  GPIO.setup(pin, GPIO.OUT, initial=0)
+
+ccw = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
+[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+
+def stepper():
+  try:
+    while(True):
+      for state in range(8):
+        for pin in range(4):
+          GPIO.output(stepperPins[pin], sequence[state][pin])
+      time.delay(1)
+  except Exception as e:
+    print(e)
+    
+
+
+stepperThread = threading.Thread(target= stepper)
+
+stepperThread.start() #continuously looping through stepper code
+while(True):
+  photoresistorValue= 
+
+
+
---
+++ door.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+
+PIRPin= 5 #change this as needed- this connects to alarm pin (low if something is detected)
+servoPin= 13 #change this as needed
+GPIO.setup(PIRPin, GPIO.IN,pull_up_down= GPIO.PUD_UP)
+GPIO.setup(servoPin, GPIO.OUT)
+dcMin = 3 #this may need to be changed
+dcMax = 12 #this may need to be changed
+servo= GPIO.PWM(servoPin, 50) #50 Hz
+
+def doorOpen():
+  servo.start(0)
+  try:
+    while True: #**** changed this to only do one rotation timing wise 
+      for dc in range(dcMin,dcMax):
+        servo.ChangeDutyCycle(dc)
+        print(dc)
+        time.sleep(0.5)
+  except KeyboardInterrupt:
+    print("bye")
+    servo.stop()
+  GPIO.cleanup()
+
+GPIO.add_event_detect(PIRPin, GPIO.RISING, callback= doorOpen)
