1c0030094c43790749551b141d72823d4be9dcf8
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-12-06 21:10:16 +0000

    trial code

---
+++ doorTrial.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+
+PIRPin= 5 #change this as needed- this connects to alarm pin (low if something is detected)
+LEDpin= 13 #change this as needed
+GPIO.setup(PIRPin, GPIO.IN,pull_up_down= GPIO.PUD_DOWN)
+GPIO.setup(LEDPin, GPIO.OUT)
+
+def doorOpen(self):
+  print('led will turn on')
+  PIRreading = GPIO.input(PIRPin)
+  try:
+    while(PIRreading>0): #**** changed this to only do one rotation timing wise 
+      GPIO.output(LEDpin, HIGH)
+      PIRreading = GPIO.input(PIRPin)
+  except KeyboardInterrupt:
+    print("bye")
+    
+  GPIO.cleanup()
+
+GPIO.add_event_detect(PIRPin, GPIO.BOTH, callback= doorOpen, bouncetime=100)
+
+while(True):
+  PIRreading = GPIO.input(PIRPin)
+  print('waiting for edge detection, PIR Reading is: ', PIRreading)
+  time.sleep(.5)
