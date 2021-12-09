aa1a738f40c1f800f5d7fba2956b98e2ae4f8225
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-12-09 15:53:33 +0000

    add delay

--- stepperTrial.py
+++ stepperTrial.py
@@ -4,13 +4,11 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-photoResistor= 0 #change as needed
-ambientVal= 100 #change depending on room lighting
-address= 0x48 #find device address
+def delay_us(tus): # use microseconds to improve time resolution
+  endTime = time.time() + float(tus)/ float(1E6)
+  while time.time() < endTime:
+    pass
 
-
-
-GPIO.setup(photoResistor, GPIO.IN)
 stepperPins = [23,24,16,20] # controller inputs: in1, in2, in3, in4
 sequence = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
         [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
@@ -26,6 +24,7 @@ try:
       for state in range(8):
         for pin in range(4):
           GPIO.output(stepperPins[pin], sequence[state][pin])
+        delay_us(1000)
       time.sleep(1)
 except Exception as e:
   print(e)
