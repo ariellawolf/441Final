f00108c2bb93af5ce3ee33e606275d238251cb80
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-12-09 14:45:52 +0000

    print

--- dispenseTrial.py
+++ dispenseTrial.py
@@ -7,11 +7,18 @@ from classes import ADC
 
 
 photoResistor= 0 #change as needed
-
+ambient val= 80
 address= 0x48 #find device address
 myADC= ADC(address)
+
 photoResVal=myADC.read(0) #0 channel
-print(photoResVal)
 
 
-
+while(True):
+  if (photoResVal> ambientVal):
+    photoResVal=myADC.read(0) #0 channel
+    print('the first cond is true')
+    print('the photo res is at: ', photoResVal)
+  elif (photoResVal< ambientVal):
+    stepperThread.end()
+    print('the second cond it true')
