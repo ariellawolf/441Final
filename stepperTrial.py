01e5d763e8902fbd0366b4ec6017d03b87f45f9c
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-12-09 17:29:16 +0000

    try

--- stepperTrial.py
+++ stepperTrial.py
@@ -1,31 +1,19 @@
 import RPi.GPIO as GPIO
 import time
-
+from classes import Stepper
 
 GPIO.setmode(GPIO.BCM)
 
-def delay_us(tus): # use microseconds to improve time resolution
-  endTime = time.time() + float(tus)/ float(1E6)
-  while time.time() < endTime:
-    pass
-
 stepperPins = [19,26,17,27] # controller inputs: in1, in2, in3, in4
-sequence = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
-        [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+
 for pin in stepperPins:
   GPIO.setup(pin, GPIO.OUT, initial=0)
 
-ccw = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
-[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+stepperTry= Stepper(180,0)
 
 try:
     while(True):
       print('in while loop')
-      for state in range(8):
-        for pin in range(4):
-          GPIO.output(stepperPins[pin], sequence[state][pin])
-          print('rotating')
-        delay_us(1000)
-      time.sleep(1)
+      stepperTry.goAngle()
 except Exception as e:
   print(e)
