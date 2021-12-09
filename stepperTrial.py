8c8ecb3d51687fbddeff3d1c1c18c8fd45433dbd
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-12-09 15:46:19 +0000

    stepper test

---
+++ stepperTrial.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+import time
+import threading
+import smbus
+from classes import ADC
+
+GPIO.setmode(GPIO.BCM)
+
+photoResistor= 0 #change as needed
+ambientVal= 100 #change depending on room lighting
+address= 0x48 #find device address
+
+
+myADC= ADC(address)
+GPIO.setup(photoResistor, GPIO.IN)
+stepperPins = [12,16,20,21] # controller inputs: in1, in2, in3, in4
+sequence = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
+        [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+for pin in stepperPins:
+  GPIO.setup(pin, GPIO.OUT, initial=0)
+
+ccw = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
+[0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+
+try:
+    while(True):
+      print('in while loop')
+      for state in range(8):
+        for pin in range(4):
+          GPIO.output(stepperPins[pin], sequence[state][pin])
+      time.delay(1)
+  except Exception as e:
+    print(e)
