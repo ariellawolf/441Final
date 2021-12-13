030a17599d6d030f22c5b10b7e689160eb62bf71
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-12-13 16:55:03 +0000

    servo rotat

--- Trial Codes/dispense.py
+++ Trial Codes/dispense.py
@@ -31,7 +31,7 @@ def stepper():
         stepperTry.contRotate()
       elif(condition== True):
         print("this second cond is true")
-        for i in range(1200):
+        for i in range(2000):
           stepperTry.contRotate()
         condition= False
       else:
