d27464a3b49189ab5e8c92750369075df393cc2b
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-12-13 17:29:01 +0000

    val

--- dispense_and_door.py
+++ dispense_and_door.py
@@ -41,7 +41,7 @@ def stepper():
         stepperTry.contRotate()
       elif(condition== True):
         print("this second cond is true")
-        for i in range(500):
+        for i in range(450):
           stepperTry.contRotate()
         condition= False
       else:
