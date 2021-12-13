1ffbd1481dd7b6fb1fa8c7c889da0d1295a8560b
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-12-13 16:07:13 +0000

    servo

---
+++ Trial Codes/servo.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+
+PIRPin= 5 #change this as needed- this connects to alarm pin (low if something is detected)
+servoPin= 13 #change this as needed
+GPIO.setup(PIRPin, GPIO.IN,pull_up_down= GPIO.PUD_DOWN)
+GPIO.setup(servoPin, GPIO.OUT)
+dcMin = 2 #this may need to be changed
+dcMax = 12 #this may need to be changed
+servo= GPIO.PWM(servoPin, 50) #50 Hz
+
+
+servo.start(dcMin)
+print('in try statement')  
+servo.ChangeDutyCycle(3)
+
+    
