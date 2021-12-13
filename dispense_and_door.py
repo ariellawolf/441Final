44280bb0cc8c9f0dc05b29cb42633397bcbb9c69
awolf24 <91500629+ariellawolf@users.noreply.github.com>
2021-12-13 17:21:34 +0000

    stepper rotations

--- dispense_and_door.py
+++ dispense_and_door.py
@@ -41,7 +41,7 @@ def stepper():
         stepperTry.contRotate()
       elif(condition== True):
         print("this second cond is true")
-        for i in range(650):
+        for i in range(450):
           stepperTry.contRotate()
         condition= False
       else:
